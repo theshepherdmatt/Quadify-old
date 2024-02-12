9c848809744507fdac162cf5932d05d73bd9f070
Quadify <matt.theshepherd@gmail.com>
2024-02-12 16:15:53 +0000

    Update start-oled.sh

--- oled/start-oled.sh
+++ oled/start-oled.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-/bin/node /home/matt/Quadify/moode/oled/index.js moode &
-# Wait for index.js to fully start. Adjust the sleep time as needed.
-sleep 5
-/bin/node /home/matt/Quadify/moode/oled/buttonsleds.js
+/bin/node /home/matt/Quadify/moode/oled/index.js moode
